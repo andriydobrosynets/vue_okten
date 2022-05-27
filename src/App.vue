9da83ce5eaaff70fd608083e97e08226b7290327
andriydobrosynets <andriy.andriy1995@gmail.com>
2022-05-28 00:29:12 +0300

    lesson1

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,55 @@
 <template>
+
+  <button v-on:click="isVisible=!isVisible">togle list visibility ({{isVisible}})</button>
+
+  <Test v-if="isVisible" v-bind:items="items" v-on:removeItem="removeItemFromList"/>
+  <h1 v-else-if="!isVisible && counter===0">some strange condition</h1>
+  <h1 v-else>list is hidden</h1>
+
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <h1>{{user.name}}</h1>
+  <div>counter value :{{counter}}</div>
+  <button v-on:click="incCounter($event,123)" >inc</button>
+  <button v-on:click="decCounter($event,123)" >dec</button>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Test from './components/Test.vue'
 
 export default {
   name: 'App',
+  data() {
+    return{
+      isVisible:true,
+      items:[{id:1,text:"text1"},{id:2,text:"text2"},{id:3,text:"text3"}],
+      counter:0,
+      userName:'andriy',
+      user:{
+        name:'lol'
+      }
+    }
+  },
+  methods:{
+    removeItemFromList(id){
+      this.items=this.items.filter(item=>item.id!==id)
+
+  },
+    incCounter(){
+      this.counter++
+      this.user.name += this.counter
+    },
+    decCounter(name){
+      this.counter--
+    }
+  },
   components: {
-    HelloWorld
+    /*HelloWorld,*/
+    Test
   }
 }
+
 </script>
 
 <style>
---
+++ src/components/Test.vue
@@ -0,0 +1,31 @@
+<template>
+<h1>TEST COMPONENT {{text}}</h1>
+  <h3 v-for="item of items" v-bind:key="item.id">
+    {{item.text}} <button v-on:click="removeItem(item.id)">remove</button>
+  </h3>
+</template>
+
+<script>
+export default {
+  name: "Test",
+  /*props:['text']*/
+  props:{
+    items:{
+      type:Array,
+      required:true
+    }
+  },
+  methods:{
+    removeItem(id){
+      this.$emit('removeItem',id)
+      //this.items=this.items.filter(item=>item.id!==id)
+      console.log(id)
+    },
+  },
+  emits:['removeItem']
+}
+</script>
+
+<style scoped>
+
+</style>
